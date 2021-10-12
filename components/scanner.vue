863e31e937859addc1a001f309c2571e6dcd1795
DESKTOP-IC0DK3R\Nimi <toluwanimiakinleye01@gmail.com>
2021-10-12 02:50:25 +0100

    added scanner frame

---
+++ components/scanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="show" class="fixed w-full h-screen bg-black bg-opacity-20 lg:hidden">
+    <div class="flex flex-col justify-center items-center">
+      <qrcode-stream></qrcode-stream>
+      <button @click="cancel">Done</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    cancel () {
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vuelidate.js' },
-    { src: '~/plugins/notifications', ssr: false }
+    { src: '~/plugins/notifications.js', ssr: false },
+    { src: '~/plugins/scanner.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
