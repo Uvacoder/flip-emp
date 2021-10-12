71cc6eda84aeaa089bdf627ad084a9ec1ba34919
DESKTOP-IC0DK3R\Nimi <toluwanimiakinleye01@gmail.com>
2021-10-12 14:32:11 +0100

    made scanner work in development

--- components/scanner.vue
+++ components/scanner.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="show" class="fixed w-full h-screen bg-black bg-opacity-20 lg:hidden">
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center p-3">
       <qrcode-stream @decode="onDecode"></qrcode-stream>
-      <button @click="cancel">Done</button>
+      <button class="my-3" @click="cancel">Cancel</button>
     </div>
   </div>
 </template>
@@ -19,7 +19,9 @@ export default {
       this.show = !this.show
     },
     onDecode (decodedString) {
-      window.location.replace(decodedString);
+      // window.location.replace(decodedString);
+      // console.log(decodedString.slice(26))
+      this.$router.push(`/${decodedString.slice(26)}`)
     }
   }
 }
